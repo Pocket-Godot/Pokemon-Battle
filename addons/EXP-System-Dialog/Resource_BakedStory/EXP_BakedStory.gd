75ee260553d7bdad3399ad6fe7bbd7b87dc6cf04
mechPenSketch <pikadon92@gmail.com>
2021-07-28 15:55:07 +0800

    Removed Plugins

--- addons/EXP-System-Dialog/Resource_BakedStory/EXP_BakedStory.gd
+++
@@ -1,6 +0,0 @@
-tool
-extends Resource
-
-export(String) var TYPE = "EXP_Baked_Story"
-export(Dictionary) var story : Dictionary
-export(Dictionary) var names : Dictionary
--- addons/EXP-System-Dialog/Resource_NodeTemplate/EXP_NodeTemplate.gd
+++
@@ -1,6 +0,0 @@
-tool
-extends Resource
-
-var TYPE = "EXP_Dialog_Node_Template_editor"
-
-export(String) var template
