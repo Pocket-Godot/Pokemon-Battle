52e6918617911b714262b9108e6726f47f8fa02b
mechPenSketch <pikadon92@gmail.com>
2021-08-04 12:24:45 +0800

    States
    Also implemented button effects for Fight, Moves' Back

---
+++ content/scenes/battle.gd
@@ -0,0 +1,34 @@
+extends Node
+
+enum STATES {COMMANDS, MOVES, TARGET, EXECUTION}
+var state = STATES.COMMANDS
+
+func _ready():
+	activate_state(state)
+
+func activate_state(s):
+	match s:
+		STATES.COMMANDS:
+			$UI_Layer/MarginContainer/Commands.show()
+			$UI_Layer/MarginContainer/Commands/Fight.grab_focus()
+		STATES.MOVES:
+			$UI_Layer/MarginContainer/Moves.show()
+			$UI_Layer/MarginContainer/Moves/List/Move1/Button.grab_focus()
+
+func change_state(s):
+	deactivate_state(state)
+	state = s
+	activate_state(state)
+
+func deactivate_state(s):
+	match s:
+		STATES.COMMANDS:
+			$UI_Layer/MarginContainer/Commands.hide()
+		STATES.MOVES:
+			$UI_Layer/MarginContainer/Moves.hide()
+
+func _command_fight():
+	change_state(STATES.MOVES)
+
+func _move_back():
+	change_state(STATES.COMMANDS)
