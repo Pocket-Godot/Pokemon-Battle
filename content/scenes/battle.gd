9f39a66d509eb523734ca6d8454ba0b3afd3d630
mechPenSketch <pikadon92@gmail.com>
2021-12-25 17:54:04 +0800

    Implemented Move Button Click

--- content/scenes/battle.gd
+++ content/scenes/battle.gd
@@ -9,7 +9,14 @@ func _ready():
 	add_child(dialogic_node)
 	move_child(dialogic_node, get_child_count() - 2)
 	dialogic_node.set_process_input(false)
+	
+	#CONNECT MOVES
+	for i in 4:
+		get_node("UI_Layer/MarginContainer/Moves/List/Move" + String(i+1) + "/Button").connect("pressed", self, "_on_move_btn_pressed", [i])
 
 func _on_dialog_completed():
 	if dialogic_node.current_event:
 		pass
+
+func _on_move_btn_pressed(i:int):
+	print(i)
