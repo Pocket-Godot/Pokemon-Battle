be4352c34671f0769ad7ee537ac8cf06c5610e04
mechPenSketch <pikadon92@gmail.com>
2022-12-06 15:19:49 +0800

    Update Data

--- data/moves/category.gd
+++ data/moves/category.gd
@@ -4,7 +4,7 @@ export(Resource) var type
 
 export(int) var base_power
 export(int) var accuracy = 100
-export(int) var power_points
+export(int) var power_points = 10
 
 func get_name()->String:
 	return resource_path.get_file().get_basename().capitalize()
