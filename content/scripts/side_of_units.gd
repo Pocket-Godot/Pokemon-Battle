52e35da66396211f3968ed08f04867b1d523edfd
mechPenSketch <pikadon92@gmail.com>
2022-12-18 23:59:38 +0800

    Set Up Sides

---
+++ content/scripts/side_of_units.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+export(Array, Resource) var species
+var units = []
+
+func _ready():
+	for r in species:
+		var dict = {}
+		
+		dict['species'] = r
+		dict['cur_hp'] = r.hp
+		
+		units += dict
